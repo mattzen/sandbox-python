e3e71c20601d2283bf67521fc9e5633971bff521
Mateusz Zmijewski <mattzy90@hotmail.com>
2023-07-14 23:02:56 -0400

    gitignore added

--- sandbox.py
+++ sandbox.py
@@ -60,4 +60,4 @@ if __name__ == "__main__":
     #print(phoneNumbers("2345"))
     #print(twoSumHashTable([1,2,3,4,55,7], 8))
     sol = SolutionHIndex()
-    print(sol.hIndex([1,3,1]))
+    print(sol.hIndex([1,10,1]))

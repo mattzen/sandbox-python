d612861945f03462089027f7ee71ebcf3156bece
Mateusz Zmijewski <mattzy90@hotmail.com>
2023-07-21 20:19:07 -0400

    min path bottom up fixed

--- recursion.py
+++ recursion.py
@@ -134,6 +134,26 @@ def minPathSumBottomUp(grid: List[List[int]]) -> int:
                 
     return dfs(grid, 0, 0)
     
+    
+    
+
+def minPathSumBottomUpDP(grid: List[List[int]]) -> int:
+    
+    row_len = len(grid[0])
+    col_len = len(grid)
+        
+    def dfs(grid, i, j):
+        #last element bottom left corner
+        if(i == col_len - 1 and j == row_len -1):
+            return grid[i][j]  
+        elif(i >= col_len or j >= row_len):
+            return sys.maxsize
+        else:
+            return grid[i][j] + min(dfs(grid, i + 1, j),
+                                    dfs(grid, i, j + 1))
+            
+                
+    return dfs(grid, 0, 0)
 
 def minPathSumTopDown(grid: List[List[int]]) -> int:
     def minCost(cost, m, n):
@@ -147,6 +167,18 @@ def minPathSumTopDown(grid: List[List[int]]) -> int:
     return minCost(grid, len(grid)-1, len(grid[0])-1)
         
 
+print(minPathSumBottomUpDP([[7,1,3,5,8,9,9,2,1,9,0,8,3,1,6,6,9,5],
+                            [9,5,9,4,0,4,8,8,9,5,7,3,6,6,6,9,1,6],
+                            [8,2,9,1,3,1,9,7,2,5,3,1,2,4,8,2,8,8],
+                            [6,7,9,8,4,8,3,0,4,0,9,6,6,0,0,5,1,4],
+                            [7,1,3,1,8,8,3,1,2,1,5,0,2,1,9,1,1,4],
+                            [9,5,4,3,5,6,1,3,6,4,9,7,0,8,0,3,9,9],
+                            [1,4,2,5,8,7,7,0,0,7,1,2,1,2,7,7,7,4],
+                            [3,9,7,9,5,8,9,5,6,9,8,8,0,1,4,2,8,2],
+                            [1,5,2,2,2,5,6,3,9,3,1,7,9,6,8,6,8,3],
+                            [5,7,8,3,8,8,3,9,9,8,1,9,2,5,4,7,7,7],
+                            [2,3,2,4,8,5,1,7,2,9,5,2,4,2,9,2,8,7],
+                            [0,1,6,1,1,0,0,6,5,4,3,4,3,7,9,6,1,9]])) 
 
 print(minPathSumBottomUp([[1, 3, 1],[1, 5, 1],[4, 2, 1]])) 
 print(minPathSumBottomUp([[1, 2, 3],[4, 5, 6]])) 

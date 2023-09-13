99a9411928ac740b46115d00766ad5a36bc18c85
you@example.com <you@example.com>
2023-09-13 08:01:11 -0400

    refactor

--- leetcode/matrix.py
+++ leetcode/matrix.py
@@ -19,12 +19,7 @@ class SolutionMatrixRotate90:
                 self.swap(matrix, dim, dim - index, ctr + index, dim) 
             ctr += 1
             dim -= 1
-   
-        
-        
-
-        
-        
+       
 matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 #matrix = [[1,2],[3,4]]
 
